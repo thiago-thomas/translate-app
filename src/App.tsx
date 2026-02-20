e4873f7de2bc0df6398412347cda5a1a569d41a3
Thiago Thomas <thiago_rocha17@hotmail.com>
2026-02-20 23:31:25 +0000

    feat: copy buttons of translating and translated texts

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,48 @@ import { useState } from "react";
 import { fetchTranslation } from "./services/api";
 import type { TranslationResponse } from "./types/api";
 import { Loading } from "./components/Loading";
+import { Slide, ToastContainer, toast } from "react-toastify";
 
 function App() {
-  const [translatingText, setTranslatingText] = useState("Hello World!");
+  const [translatingText, setTranslatingText] = useState("");
   const [translatingLanguage, setTranslatingLanguage] = useState("en-US");
   const [translatedText, setTranslatedText] = useState("");
   const [translatedLanguage, setTranslatedLanguage] = useState("es-ES");
   const [loading, setLoading] = useState(false);
 
   async function handleTranslate() {
-    if(translatingText) {
+    if (translatingText) {
       try {
         setLoading(true);
-        const translatedData: TranslationResponse = await fetchTranslation(translatingText, translatingLanguage, translatedLanguage);
+        const translatedData: TranslationResponse = await fetchTranslation(
+          translatingText,
+          translatingLanguage,
+          translatedLanguage,
+        );
         setTranslatedText(translatedData.matches[0].translation);
       } catch (err) {
-        alert('An error occurred while fetching translation. Error: ' + err);
+        alert("An error occurred while fetching translation. Error: " + err);
       } finally {
         setLoading(false);
       }
     } else {
-      alert('Please enter text to translate.');
+      alert("Please enter text to translate.");
     }
   }
+
+  const copyTranslatingText = async () => {
+    if (translatingText) {
+      await navigator.clipboard.writeText(translatingText);
+      toast.success("Translating text copied!");
+    }
+  };
+
+  const copyTranslatedText = async () => {
+    if (translatedText) {
+      await navigator.clipboard.writeText(translatedText);
+      toast.success("Translated text copied!");
+    }
+  };
 
   return (
     <>
@@ -42,28 +61,28 @@ function App() {
             <div className="translator__source-language">
               <button
                 type="button"
-                className={`translator__source-language-button ${translatingLanguage === 'autodetect' ? 'active' : ''}`}
+                className={`translator__source-language-button ${translatingLanguage === "autodetect" ? "active" : ""}`}
                 onClick={() => setTranslatingLanguage("autodetect")}
               >
                 Detect Language
               </button>
               <button
                 type="button"
-                className={`translator__source-language-button ${translatingLanguage === 'en-US' ? 'active' : ''}`}
+                className={`translator__source-language-button ${translatingLanguage === "en-US" ? "active" : ""}`}
                 onClick={() => setTranslatingLanguage("en-US")}
               >
                 English
               </button>
               <button
                 type="button"
-                className={`translator__source-language-button ${translatingLanguage === 'fr-FR' ? 'active' : ''}`}
+                className={`translator__source-language-button ${translatingLanguage === "fr-FR" ? "active" : ""}`}
                 onClick={() => setTranslatingLanguage("fr-FR")}
               >
                 French
               </button>
               <button
                 type="button"
-                className={`translator__source-language-button ${translatingLanguage === 'es-ES' ? 'active' : ''}`}
+                className={`translator__source-language-button ${translatingLanguage === "es-ES" ? "active" : ""}`}
                 onClick={() => setTranslatingLanguage("es-ES")}
               >
                 Spanish
@@ -81,12 +100,16 @@ function App() {
                 <button type="button">
                   <img src={listenIcon} alt="Listen" />
                 </button>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={copyTranslatingText}
+                  disabled={!translatingText}
+                >
                   <img src={copyIcon} alt="Copy" />
                 </button>
               </div>
               <div className="translator__source-options-translate">
-                <button type="button" onClick={handleTranslate}>
+                <button type="button" onClick={handleTranslate} disabled={!translatingText}>
                   <img src={alfaIcon} alt="Translate" />
                   Translate
                 </button>
@@ -98,21 +121,21 @@ function App() {
               <div className="translator__target-language-left">
                 <button
                   type="button"
-                  className={`translator__target-language-button ${translatedLanguage === 'en-US' ? 'active' : ''}`}
+                  className={`translator__target-language-button ${translatedLanguage === "en-US" ? "active" : ""}`}
                   onClick={() => setTranslatedLanguage("en-US")}
                 >
                   English
                 </button>
                 <button
                   type="button"
-                  className={`translator__target-language-button ${translatedLanguage === 'fr-FR' ? 'active' : ''}`}
+                  className={`translator__target-language-button ${translatedLanguage === "fr-FR" ? "active" : ""}`}
                   onClick={() => setTranslatedLanguage("fr-FR")}
                 >
                   French
                 </button>
                 <button
                   type="button"
-                  className={`translator__target-language-button ${translatedLanguage === 'es-ES' ? 'active' : ''}`}
+                  className={`translator__target-language-button ${translatedLanguage === "es-ES" ? "active" : ""}`}
                   onClick={() => setTranslatedLanguage("es-ES")}
                 >
                   Spanish
@@ -135,13 +158,30 @@ function App() {
                 <button type="button">
                   <img src={listenIcon} alt="Listen" />
                 </button>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={copyTranslatedText}
+                  disabled={!translatedText}
+                >
                   <img src={copyIcon} alt="Copy" />
                 </button>
               </div>
             </div>
           </div>
         </div>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Slide}
+        />
       </main>
     </>
   );
